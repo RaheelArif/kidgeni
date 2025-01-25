db1fc4320369e27be679195dc70dcf65e036e812
Raheel Arif <phuawie292@gmail.com>
2025-01-25 20:00:25 +0500

    faq

---
+++ src/app/faq/page.js
@@ -0,0 +1,14 @@
+import PrivacyPolicy from "@/components/Faq/PrivacyPolicy";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <Header />
+      <PrivacyPolicy />
+      <Footer />
+    </div>
+  );
+}
