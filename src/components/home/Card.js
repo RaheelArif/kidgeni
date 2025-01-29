da54ca7d696bb1e2ae1f4c1b60a6e53112aa0aa2
Raheel Arif <phuawie292@gmail.com>
2025-01-30 00:26:03 +0500

    product details

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -1,24 +1,26 @@
 import React from "react";
 import styles from "./Card.module.scss";
 import Image from "next/image";
+import Link from 'next/link';
 
-
-export default function Card({product}) {
+export default function Card({ product }) {
   return (
-    <div className={styles.productCard}>
-      <div className={styles.imageContainer}>
-        <span className={styles.inStock}>In-Stock</span>
-        <Image
-          src={product.image}
-          alt={product.title}
-          className={styles.productImage}
-        />
+    <Link href="/product-detail" passHref>
+      <div className={styles.productCard}>
+        <div className={styles.imageContainer}>
+          <span className={styles.inStock}>In-Stock</span>
+          <Image
+            src={product.image}
+            alt={product.title}
+            className={styles.productImage}
+          />
+        </div>
+        <div className={styles.productDetails}>
+          <p className={styles.productCat}>Lorem Ipsum</p>
+          <p className={styles.productTitle}>{product.title}</p>
+          <p className={styles.productPrice}>{product.price}</p>
+        </div>
       </div>
-      <div className={styles.productDetails}>
-        <p className={styles.productCat}>Lorem Ipsum</p>
-        <p className={styles.productTitle}>{product.title}</p>
-        <p className={styles.productPrice}>{product.price}</p>
-      </div>
-    </div>
+    </Link>
   );
 }
