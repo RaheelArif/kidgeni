4640744c0984879c9d8a2c375895784cbb619aa1
Raheel Arif <phuawie292@gmail.com>
2025-02-19 19:06:00 +0500

    term

--- src/app/terms-of-use/page.js
+++ src/app/terms-of-use/page.js
@@ -1,15 +1,14 @@
-import ContactForm from "@/components/ContactUs/ContactForm";
+import PrivacyPolicy from "@/components/Faq/PrivacyPolicy";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import Terms from "@/components/terms/Terms";
 import React from "react";
 
-export default function Contact() {
+export default function page() {
   return (
     <div>
       <Header />
-      <div className="contact-form-p">
-        <ContactForm />
-      </div>
+      <Terms />
       <Footer />
     </div>
   );
---
+++ src/components/terms/Terms.js
@@ -0,0 +1,224 @@
+"use client";
+import React from "react";
+import { Typography, Space } from "antd";
+import styles from "./Terms.module.scss";
+
+const { Title, Paragraph } = Typography;
+
+const Terms = () => {
+  return (
+    <div>
+      <div className={styles.privacyPolicyContainer1}>
+        <Title level={1} className={styles.privacyPolicyTitle1}>
+          Terms of Use
+        </Title>
+        <Paragraph className={styles.privacyPolicyParagraph1}>
+          At Lorem Ipsum we intend to give you as much control as possible over
+          your personal information, including the registration data. We will
+          let each user know how we will use the personal information derived
+          from each user's access to and use of website and/or the services.
+        </Paragraph>
+      </div>
+      <div className={styles.privacyPolicyContainer}>
+        <Title level={2} className={styles.sectionTitle}>
+          CONTROLLER
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          The controller is the entity within PowerChap Group through which
+          websites the Personal Data concerned have been collected. You will
+          find the full contact details of the controller in the footer of its
+          website. The controller is referred to as “PowerChap” throughout this
+          document.
+        </Paragraph>
+
+        <Title level={2} className={styles.sectionTitle}>
+          PERSONAL DATA WE COLLECT ABOUT YOU
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          If possible, we will try to collect the personal data directly from
+          the person concerned. However, it may be necessary to collect personal
+          data from third parties in the context of future relationships (e.g.
+          customers or candidates).
+        </Paragraph>
+
+        <Title level={3} className={styles.subsectionTitle}>
+          I. Information you provide us:
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          Cookies are files with a small amount of data that is commonly used as
+          an anonymous unique identifier. These are sent to your browser from
+          the website that you visit and are stored on your computer’s hard
+          drive. Our website uses these “cookies” to collect information and to
+          improve our Service. You have the option to either accept or refuse
+          these cookies, and know when a cookie is being sent to your computer.
+          If you choose to refuse our cookies, you may not be able to use some
+          portions of our Service.
+        </Paragraph>
+        <Paragraph className={styles.paragraph}>
+          <ul>
+            <li>
+              {" "}
+              contact form filled out by you (website): name, email, company,
+              phone, function, subject and - message;
+            </li>
+            <li>
+              {" "}
+              job applications (websites or via recruitment company):
+              application forms, CV, interview notes and related recruitment
+              information;
+            </li>
+            <li> your feedback;</li>
+            <li>
+              {" "}
+              content of correspondence with us (by email, phone, letter, ...);
+            </li>
+            <li> subscription to newsletters;</li>
+            <li>
+              {" "}
+              information from social media when you link to our website
+              (online)
+            </li>
+            <li>
+              {" "}
+              information relating to your use of our products or services
+            </li>
+            <li> contact details of past, present and future customers</li>
+            <li>
+              {" "}
+              contact details of past, present and future suppliers, vendors,
+              investors or other stakeholders
+            </li>
+          </ul>
+        </Paragraph>
+
+        <Title level={3} className={styles.subsectionTitle}>
+          II. Information we may automatically collect if you visit our website:
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          technical information, including IP address, login information,
+          browser type and version cookies and other technologies (See our
+          Cookie Policy on our website for more info.) PowerChap will track the
+          domains from which people visit PowerChap and analyze this data for
+          statistical purposes. Subject to the provisions of this Privacy
+          Policy, different PowerChap sites may use accumulated data for
+          different purposes, including but not limited to marketing analysis,
+          service evaluation and planning.
+        </Paragraph>
+
+        <Title level={3} className={styles.subsectionTitle}>
+          III. Use of children’s data:
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          PowerChap does not knowingly collect, process or store any personal
+          data about children under the age of 16 and we do not offer any
+          products or services directly to children under the legally allowed
+          age.
+        </Paragraph>
+        <Paragraph className={styles.paragraph}>
+          Finally, information collected on our site may be stored and processed
+          into anonymous statistics or log files for internal use.
+        </Paragraph>
+
+        <Title level={2} className={styles.sectionTitle}>
+          PURPOSES
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          In general, we may have to process all categories of personal data to
+          comply with applicable legal or regulatory requirements, relevant
+          industry standards and our policies, and for litigation or defense of
+          claims.
+        </Paragraph>
+
+        <Title level={3} className={styles.subsectionTitle}>
+          customers’ and prospects’ personal data:
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          We use your personal data for the performance of your contract(s) with
+          PowerChap or business development purposes. In particular, we process
+          customers’ personal data to deliver and provide our products or
+          services and to invoice them, as well as for the execution and
+          analysis of market surveys and marketing strategies.
+        </Paragraph>
+        <Paragraph className={styles.paragraph}>
+          We also use your contact details to further inform prospects and
+          customers about our products and services that are in line with our
+          already existing relationship and for marketing such as offering
+          promotions.
+        </Paragraph>
+
+        <Title level={3} className={styles.subsectionTitle}>
+          Suppliers’, vendors, journalists and other stakeholders personal data:
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          We process your personal data to conclude and execute our agreements
+          and manage our relationships.
+        </Paragraph>
+
+        <Title level={3} className={styles.subsectionTitle}>
+          Investors’ and shareholders’ data:
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          We use your personal data in order to manage our relationship and our
+          obligations. We maintain contact details for external communications
+          and to respond to your queries.
+        </Paragraph>
+        <Title level={3} className={styles.subsectionTitle}>
+          Students and Job seekers’ data:
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          Your personal data are stored for the recruitment and selection
+          procedures. More specifically, your personal data are processed for
+          the purposes of assessing your profile and skills to fit a job and
+          making you specific job offers.
+        </Paragraph>
+        <Paragraph className={styles.paragraph}>
+          Subject to your explicit consent, we will use your contact details to
+          send you our newsletter, mailing and invite you to events. We also may
+          use the personal data in other ways, for which we provide specific
+          notice at the time of collection.
+        </Paragraph>
+        <Paragraph className={styles.paragraph}>
+          Other Notification: In order to implement or enforce the Terms of
+          Service of the relevant PowerChap site, PowerChap may use personal
+          data to contact users on an individual basis.
+        </Paragraph>
+
+        <Title level={2} className={styles.sectionTitle}>
+          RETENTION OF YOUR PERSONAL DATA
+        </Title>
+        <Paragraph className={styles.paragraph}>
+          Your personal data is kept in our information systems only for as long
+          as is necessary for the purposes for which it was collected (as
+          described above). It will be deleted once it is no longer needed by us
+          and once all requirements at law have been met.
+        </Paragraph>
+        <Paragraph className={styles.paragraph}>
+          We will retain and use your personal data for as long as reasonably
+          necessary to perform our agreements or to consider tenders, to comply
+          with our legal obligations (such as accounting and tax obligations)
+          and to resolve disputes or enforce our agreements. Therefore, your
+          personal data will be held during the duration of our contractual
+          relationship and up to 10 years thereafter.
+        </Paragraph>
+
+        <Paragraph className={styles.paragraph}>
+          Application forms (CV’s), interview notes and references of
+          unsuccessful candidates are kept for a period of 24 months following
+          the closure of the hiring process. When we would like to stay in
+          contact with an applicant who was not chosen for the job he/she
+          applied for we will ask for his/her consent to keep the cv in our
+          application pool for another 2 years.
+        </Paragraph>
+        <Paragraph className={styles.paragraph}>
+          In all cases personal data may be kept for a longer period of time
+          where there is a legal or regulatory reason to do so, or a shorter
+          period where the individual objects to the processing of their
+          personal data and there is no longer a legitimate purpose to retain
+          it.
+        </Paragraph>
+      </div>
+    </div>
+  );
+};
+
+export default Terms;
